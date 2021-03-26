7b07dc0ff902db9aa2f96d4ea12218464b9822df
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-26 20:30:12 +0000

    new get datsa

--- 02a_sklearn_getdata.py
+++ 02a_sklearn_getdata.py
@@ -1,8 +1,3 @@
-import pandas as pd
-import os 
-os.system('kaggle datasets download -d uciml/pima-indians-diabetes-database')
-os.system('move pima-indians* ./data')
-
 import zipfile
 with zipfile.ZipFile("./data/pima-indians-diabetes-database.zip","r") as zip_ref:
     zip_ref.extractall("./data/")
