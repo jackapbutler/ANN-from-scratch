fb1c0766cb360a81d2635d52fa1875db598112e5
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-26 17:10:53 +0000

    sklearn updates

--- 02_sklearn_MLP.py
+++ 02_sklearn_MLP.py
@@ -1,2 +1,31 @@
+# Packages
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 import pandas as pd
+
+# set seed
+seed = 32
+
+# Data
+X = pd.read_csv('./data/diabetes.csv')
+y = X.pop('Outcome') # ejects quality column as labels
+
+# Train / Test Split
+X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.25, random_state=seed)
+
+# Fit model
+mlp = MLPClassifier(hidden_layer_sizes=[50, 100, 150, 100, 50], random_state=seed)
+mlp.fit(X_tr, y_tr)
+
+# Make predictions
+train_preds = mlp.predict(X_tr)
+test_preds = mlp.predict(X_te)
+
+# Results
+train_score = accuracy_score(y_tr, train_preds)
+test_score = accuracy_score(y_te, test_preds)
+print(mlp.loss_curve_)
+
+# Write this to 
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Building an artificial neural network using only the NumPy Python library along with MLOps fundamentals.<br>
 
 ## Perceptron
-![Image of network](perceptron.PNG)
+![Image of network](images/perceptron.PNG)
 
 ## MLP
-![Image of network](mlp.PNG)
+![Image of network](images/mlp.PNG)
 
 ## Sklearn's MLP and MLOps Workflow
