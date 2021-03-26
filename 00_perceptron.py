148226416a8bc3bc274ace64fd542e03263b04c6
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-26 13:46:52 +0000

    handwritten + understanding perceptron

--- 00_perceptron.py
+++ 00_perceptron.py
@@ -12,7 +12,9 @@ input_data = np.array([[0,1,0],
 labels = np.array([[1, 0, 0, 1, 1, 0, 1]]).reshape(7,1)
 
 # Initial Parameters
+np.random.seed(3)
 neuron_weights = np.random.rand(3,1)
+print(neuron_weights)
 bias = np.random.rand(1) # needed to add to the sumproduct of neurons
 learning_rate = 0.05
 
@@ -24,38 +26,33 @@ def sigmoid_derivative(x):
     return sigmoid(x)*(1-sigmoid(x))
 
 # Training session for Basic ANN
-for epoch in range(10000):
+for epoch in range(100):
 
     # Forward feeding process
     inputs = input_data
     XW = np.dot(inputs, neuron_weights) + bias # w1.x1 + w2.x2 .... for all neurons
-    z = sigmoid(XW) # returns the result to within [0,1] (because of labels)
-    error = z - labels # find error
-    print(error.sum())
+    output = sigmoid(XW) # returns the result to within [0,1] (because of labels)
+    error = output - labels # find error
+    #print("Error at epoch "+str(epoch)+": "+str(round(error.sum(), 4)))
 
     # Backpropagation
-    dpred = sigmoid_derivative(z)
-    z_del = error * dpred
+    cost = error # cost function can be changed
+    dpred = sigmoid_derivative(output) # finds the slope at each network output point
+    z_del = cost * dpred # multiplies the slope by the error terms
     inputs = input_data.T
-    neuron_weights = neuron_weights - learning_rate*np.dot(inputs, z_del)
+    neuron_weights = neuron_weights - learning_rate*np.dot(inputs, z_del) # Gradient descent
     
     for num in z_del:
         bias = bias - learning_rate*num
-
-
-print("Final error on each sample")
-print(error)
 
 # Make Predictions (should be near 0)
 test_input = np.array([1,0,0])
 net_output = np.dot(test_input, neuron_weights) + bias
 result = sigmoid(net_output)
-print(result)
 
 # Make Predictions (should be near 1)
 test_input = np.array([0,1,0])
 net_output = np.dot(test_input, neuron_weights) + bias
 result = sigmoid(net_output)
-print(result)
 
 
