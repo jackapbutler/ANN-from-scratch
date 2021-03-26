7373153b83c799d51e071e5b0f8332eb252a94c6
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-26 19:59:09 +0000

    Update 02a_sklearn_getdata.py

--- 02a_sklearn_getdata.py
+++ 02a_sklearn_getdata.py
@@ -6,5 +6,3 @@ os.system('move pima-indians* ./data')
 import zipfile
 with zipfile.ZipFile("./data/pima-indians-diabetes-database.zip","r") as zip_ref:
     zip_ref.extractall("./data/")
-
-os.system('del .\data\pima-indians-diabetes-database.zip*')
