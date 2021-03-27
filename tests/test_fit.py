e736b23206f579673c592346f8c388fcf52010bd
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-27 10:30:32 +0000

    change make_reg to make_clas

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -5,7 +5,7 @@ import json
 model = pickle.load(open("../models/sklearn_neuralnet.pkl", "rb"))
 
 # Generate some data for validation
-X_test, y = make_regression(n_samples= 1000, n_features= 9, n_classes= 2)
+X_test, y = make_classification(n_samples= 1000, n_features= 9, n_classes= 2)
 
 # Test that the model can predict
 y_hat = model.predict(X_test)
