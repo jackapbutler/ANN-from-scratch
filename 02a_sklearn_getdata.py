39f2b22bb38cf7e4ce812aa71317ca8ad49f5290
Jack Butler <47942453+jackapbutler@users.noreply.github.com>
2021-03-27 16:13:20 +0000

    ann

--- 02a_sklearn_getdata.py
+++
@@ -1,3 +0,0 @@
-import zipfile
-with zipfile.ZipFile('./data/pima-indians-diabetes-database.zip','r') as zip_ref:
-    zip_ref.extractall("./data/")
--- 02b_sklearn_processdata.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-data = pd.read_csv('./data/diabetes.csv')
-data.to_csv('./data/processed_data.csv', index=False)
